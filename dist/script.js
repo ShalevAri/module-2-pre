a374a54b9371a403dc745253d6a7e1b32b0f9486
shalev <shalevari2008@gmail.com>
2023-11-20 20:12:54 +0200

    feat(elements): get all

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+var pageTitle = document.querySelector(".page-title");
+var createActivityGroupTitle = document.querySelector(".create-activity-title");
+var ChooseActivityGroupTitle = document.querySelector(".choose-activity-title");
+var howLongTitle = document.querySelector(".how-long-title");
+var confirmButton = document.querySelector(".confirm-button");
+var cancelButton = document.querySelector(".cancel-button");
---
+++ script.ts
@@ -0,0 +1,17 @@
+const pageTitle = document.querySelector(".page-title") as HTMLHeadElement
+const createActivityGroupTitle = document.querySelector(
+	".create-activity-title"
+) as HTMLHeadElement
+const ChooseActivityGroupTitle = document.querySelector(
+	".choose-activity-title"
+) as HTMLHeadElement
+const howLongTitle = document.querySelector(
+	".how-long-title"
+) as HTMLHeadElement
+
+const confirmButton = document.querySelector(
+	".confirm-button"
+) as HTMLButtonElement
+const cancelButton = document.querySelector(
+	".cancel-button"
+) as HTMLButtonElement
